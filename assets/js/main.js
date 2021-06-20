3af5707c4f75c91c298a02023bc7588245089208
GERMAN <m_anterola@hotmail.com>
2021-06-20 11:51:09 -0400

    IMPLEMENTOS

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+// COMENTARIOS EN FAVICON //
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+  // POPOVER EN TARJETAS //
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
+
+
+  
+
+
+  
